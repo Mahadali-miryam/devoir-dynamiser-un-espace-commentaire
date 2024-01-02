871b5437ce8a146011f973ee451b330380ed5e2c
Mahadali Miryam <mahadali-miryam@outlook.com>
2024-01-02 12:23:47 +0100

    modification pour l'apparition du message d'erreur, non visible lors des enregistrements précedent

--- script.js
+++ script.js
@@ -9,13 +9,9 @@ commentForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
 //récupérer les valeurs du champs du formulaire
-let firstName = document.getElementById("first-name");
-let lastName = document.getElementById("last-name");
-let message = document.getElementById("message");
-
-let firstName = firstNameTag.value;
-let lastName = lastNameTag.value;
-let message = messageTag.value;
+let firstName = document.getElementById("first-name").value;
+let lastName = document.getElementById("last-name").value;
+let message = document.getElementById("message").value;
     
 //valider les champs du formulaire
 if (firstName.trim() === '' || lastName.trim() === '' || message.trim() === '') {
@@ -25,16 +21,16 @@ if (firstName.trim() === '' || lastName.trim() === '' || message.trim() === '') 
 } else { 
 // Création d'un nouvel élément de commentaire
 let newComment = document.createElement('div');
-    newComment.classList.add('flex', 'space-x-4', 'text-sm', 'text-gray-500');
+        newComment.classList.add('flex', 'space-x-4', 'text-sm', 'text-gray-500');
     
-    newComment.innerHTML = `
-        <div class="flex-1 py-10 border-t border-gray-200">
-            <h3 class="font-medium text-gray-900">${firstName} ${lastName}</h3>
-                <div class="prose prose-sm mt-4 max-w-none text-gray-500">
-                    <p>${message}</p>
-                </div>
-            </div>
-         `;
+            newComment.innerHTML = `
+                <div class="flex-1 py-10 border-t border-gray-200">
+                     <h3 class="font-medium text-gray-900">${firstName} ${lastName}</h3>
+                        <div class="prose prose-sm mt-4 max-w-none text-gray-500">
+                            <p>${message}</p>
+                        </div>
+                    </div>
+                 `;
     
 // Ajout du nouveau commentaire à la liste existante
 commentList.appendChild(newComment);
@@ -44,6 +40,7 @@ commentForm.reset();
     
 // Cacher le message d'erreur 
 errorMessage.style.display = "none";
+
 console.log(firstName, lastName, message)
 }
 });
